292203ae013dfac389d9006fcf3e4009e00ed7f6
Francis Marineau <frank.marineau@gmail.com>
2025-09-10 19:01:25 -0400

    remove fluff

--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -9,9 +9,6 @@ export const Route = createRootRoute({
         <Link to="/" className="[&.active]:font-bold">
           Home
         </Link>
-        <Link to="/posts" className="[&.active]:font-bold">
-          Posts
-        </Link>
       </div>
       <hr />
       <Outlet />
